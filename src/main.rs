822b1d87d26030bc7ecfc39b9ebfa414a85f18b5
Guru Senthil <guru.koushik@gmail.com>
2023-10-01 22:19:33 -0700

    feat: add subcommands to cli app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Welcome to the streak app!");
+use clap::{Parser, Subcommand};
+
+/// streak (noun) [/strēk/]: a continuous period of specified success or luck.
+/// Create, view and track streaks to develop lasting habits by creating 
+/// positive reward signals. 
+#[derive(Debug, Parser)]
+#[clap(name = "streak", version)]
+pub struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Create new streak
+    Create {
+        /// Name of the habit to create a streak
+        name: String,
+    },
+    /// Log streak for the day
+    Log {
+        /// Name of the habit to log
+        name: String,
+    },
+}
+
+fn main () {
+    let app = App::parse();
+    println!("{:?}", app);
 }
